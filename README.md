23aab99a7432321be0f5e2e7c30a01c9cec60f6a
lolita89 <cika.lolita@hotmail.com>
2018-10-04 21:03:45 +0200

    new feature: scroll up to the top of the page

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # portfolio-cika
 
-This is the webpage I made myself with HTML5, CSS3, SCSS, and vanila Javascript. I sometimes update and develop with the new features as I learn something more.
+This is the webpage I made myself from scratch with HTML5, CSS3, SCSS, and vanila Javascript. I sometime update and develop with the new features as I learn something more.
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -15,6 +15,23 @@ function responsiveNav() {
     }
 };
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 //  //portfolio boxes
 //  var boxesPortofolio = `<div class="port-box">My Project</div>`
 // document.getElementById("portBoxes").innerHTML = boxesPortofolio.repeat(2);
